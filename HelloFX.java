c29cb09f3264c05e3711f8a81768294b1e60ff23
Nithin Alagar <343954178@tdsb.ca>
2026-06-02 15:39:37 +0000

    Nithin.A

--- HelloFX.java
+++ HelloFX.java
@@ -18,15 +18,20 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.input.MouseEvent;
 
 
 public class HelloFX extends Application{
     
+    boolean pieceSelected = false;
+    int selectedPieceX, selectedPieceY;
+
     @Override
     public void start(Stage mainGameStage) {
 
@@ -35,15 +40,31 @@ public class HelloFX extends Application{
         Button start = new Button("Start Game");
 
 
-        Label gameDescription = new Label("Welcome to Pass-The-Device two player checkers! \n Click Start to play.");
-        HBox startMenuLayout = new HBox(20);
-        startMenuLayout.getChildren().addAll(start, gameDescription);
+        Label gameDescription1 = new Label("Welcome to Pass-The-Device two player checkers!");
+        Label gameDescription2 = new Label("Click Start to play.");
+        BorderPane startMenuLayout = new BorderPane();
+        startMenuLayout.setTop(gameDescription1);
+        startMenuLayout.setCenter(gameDescription2);
+        startMenuLayout.setBottom(start);
         Scene startMenu, mainGame, endScreen;
         // Label playerTurn = new Label(playerToMove + " to move.");
         // mainGame = new Scene(playerToMove, 500, 500);
         startMenu = new Scene(startMenuLayout, 500, 500);
         mainGameStage.setScene(startMenu);
         mainGameStage.show();
+        String[][] board = {
+            {"", "⛀", "", "⛀", "", "⛀", "", "⛀"},
+            {"⛀", "", "⛀", "", "⛀", "", "⛀", ""},
+            {"", "⛀", "", "⛀", "", "⛀", "", "⛀"},
+            {"", "", "", "", "", "", "", ""},
+            {"", "", "", "", "", "", "", ""},
+            {"⛂", "", "⛂", "", "⛂", "", "⛂", ""},
+            {"", "⛂", "", "⛂", "", "⛂", "", "⛂"},
+            {"⛂", "", "⛂", "", "⛂", "", "⛂", ""}
+        };
+
+
+
 
         GridPane grid = new GridPane();
         for (int row = 0; row < 8; row++){
@@ -51,35 +72,40 @@ public class HelloFX extends Application{
                 StackPane tile = new StackPane();
                 Rectangle color = new Rectangle(70, 70);
                 if ((col+row)%2 == 0){
-                    color.setFill(Color.WHITE);
+                    color.setFill(Color.BEIGE);
                 } else {
-                    color.setFill(Color.BLACK);
+                    color.setFill(Color.BROWN);
                 }
                 tile.getChildren().add(color);
                 // Adding a piece if there is one
-                String pieceCode = board[col][row];
-                if (!pieceCode.equals("")){
-                    Label piece = new Label(board[col][row]);
-                
-                piece.setStyle("-fx-font-size: 40;");
-                tile.getChildren().add(piece);
+                //String pieceCode = board[col][row];
+                if (board[col][row].equals("⛀")){
+                    Circle piece = new Circle(25, Color.BLACK);
+                    tile.getChildren().add(piece);
+                } else if (board[col][row].equals("⛂")){
+                    Circle piece = new Circle(25, Color.WHITE);
+                    tile.getChildren().add(piece);
                 }
                 grid.add(tile, row, col);
+                final int tileRow = row;
+                final int tileCol = col;
+                tile.setOnMousePressed(MouseEvent -> {
+                    if (!pieceSelected && !board[tileCol][tileRow].equals("")) {
+                        selectedPieceX = tileCol;
+                        selectedPieceY = tileRow;
+                    } else {
+                        //movePiece(selectedPieceX, selectedPieceY, tileCol, tileRow);
+                        pieceSelected = false;
+                    }
+                });
             }
         }
-        
+        mainGame = new Scene(grid, 700, 700);
         start.setOnAction(event -> {
             System.out.println("but...");
 
             mainGameStage.setScene(mainGame);
         });
-
-        
-
-
-
-        // here we add everything together
-        // Scene scene = new Scene(layout, 640, 480);
     }
 
 
